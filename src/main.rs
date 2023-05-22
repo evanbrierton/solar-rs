6088f92110179c4091b569ac162d77471efe98ba
Evan Brierton <evan@brierton.ie>
2023-05-22 18:22:51 +0100

    feat: add error checking to main method

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,20 @@
 use solar_rs::solar_data::SolarData;
 use std::fs::read_dir;
 
-fn main() {
-    let dir = read_dir("data").unwrap();
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args = std::env::args().collect::<Vec<String>>();
+    if args.len() != 2 {
+        println!("Usage: solar-rs <path>");
+        return Ok(());
+    }
+
+    let dir = read_dir(&args[1]).map_err(|e| match e.kind() {
+        std::io::ErrorKind::NotFound => format!("{}: No such file or directory", &args[1]),
+        _ => format!("{}: {}", &args[1], e),
+    })?;
+
     let data = SolarData::from(dir);
 
-    println!("{}", data)
+    println!("{}", data);
+    Ok(())
 }
